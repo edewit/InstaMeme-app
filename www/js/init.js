f1546d9ca4769fca2477120eaef4dde1a67dffb8
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-09-28 16:16:01 +0200

    browserify and npm dependencies

---
+++ www/js/init.js
@@ -0,0 +1,7 @@
+var angular = require("angular");
+require("angular-animate");
+require("angular-resource");
+require("angular-ui-router");
+require("ionic-scripts");
+require("angular-route");
+require("angular-sanitize");
